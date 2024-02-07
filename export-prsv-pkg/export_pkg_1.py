0347eb332b6661cf9de9620ae741119ad44d25ae
hshiue <hilaryszuyinshiue@nypl.org>
2024-02-07 11:04:39 -0500

    remove comments and unused module ET

--- export-prsv-pkg/export_pkg_1.py
+++ export-prsv-pkg/export_pkg_1.py
@@ -2,7 +2,6 @@ import requests
 from pathlib import Path
 import time
 import logging
-import xml.etree.ElementTree as ET
 import sys
 
 logging.basicConfig(level=logging.INFO)
@@ -45,47 +44,8 @@ def create_token(credential_set: str, token_file: Path) -> str:
 
     return data["token"]
 
-# def request_api_call(type: str, accesstoken: str, url: str) -> requests.Response:
-#     headers
-
-
-
-
-
-
-'''
-curl -X 'POST' \
-  'https://nypl.preservica.com/api/entity/structural-objects/d8ba26ae-e915-4071-8d27-4838ee73f0d7/exports' \
-  -H 'accept: text/plain;charset=UTF-8' \
-  -H 'Content-Type: application/xml;charset=UTF-8' \
-  -d '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<ExportAction xmlns="http://preservica.com/EntityAPI/v6.12" xmlns:xip="http://preservica.com/XIP/v6.12">
-    <IncludeContent>Content</IncludeContent>
-    <IncludeMetadata>Metadata</IncludeMetadata>
-    <IncludedGenerations>All</IncludedGenerations>
-    <IncludeParentHierarchy>false</IncludeParentHierarchy>
-</ExportAction>'
-
-'''
-
-"""
-1. POST
-https://nypl.preservica.com/api/entity/structural-objects/d8ba26ae-e915-4071-8d27-4838ee73f0d7/exports
-header needs to include XML
-This returns a progress token
-
-2. Check status
-https://nypl.preservica.com/api/entity/progress/{8ba8b96d-a60c-466d-9bd5-73ae7ea7325e}?includeErrors=true
-
-3. GET
-https://nypl.preservica.com/api/entity/actions/exports/8ba8b96d-a60c-466d-9bd5-73ae7ea7325e/content
-"""
-
 def main():
-    """
-    generate token
-    API
-    """
+
     user = input("Enter user name: ")
     pw = input("Enter password: ")
     tenant = input("Enter tenant (nypl or nypltest)")
